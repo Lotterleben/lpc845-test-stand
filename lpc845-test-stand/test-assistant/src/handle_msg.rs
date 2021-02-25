75de936e2e1ff1610d5077ba5d04bbd751ea0461
Lotte Steenbrink <lotte.steenbrink@ferrous-systems.com>
2021-02-25 10:23:13 +0100

    fix fmt

--- lpc845-test-stand/test-assistant/src/handle_msg.rs
+++ lpc845-test-stand/test-assistant/src/handle_msg.rs
@@ -1,30 +1,22 @@
-use heapless::{consts::U4, consts::U8, FnvIndexMap};
-use lpc8xx_hal::pac::USART0;
-use firmware_lib::usart::Tx;
-use lpc8xx_hal::usart::state::AsyncMode;
-use lpc8xx_hal::{
-    prelude::*,
-    cortex_m::interrupt,
-    gpio,
+use crate::{
+    handle_pin_interrupt, handle_pin_interrupt_dynamic, handle_pin_interrupt_noint_dynamic,
+    CTS_PIN_NUMBER, PININT0_DYN_PIN, RED_LED_PIN_NUMBER, RTS_PIN_NUMBER,
 };
-use rtt_target::rprintln;
+use firmware_lib::usart::Tx;
+use heapless::{consts::U4, consts::U8, FnvIndexMap};
+use lpc845_messages::{
+    pin, AssistantToHost, DynamicPin, HostToAssistant, InputPin, OutputPin, UsartMode,
+};
 #[cfg(feature = "sleep")]
 use lpc8xx_hal::cortex_m::asm;
-use lpc845_messages::{AssistantToHost, HostToAssistant, InputPin, OutputPin, DynamicPin, UsartMode, pin};
+use lpc8xx_hal::pac::USART0;
+use lpc8xx_hal::usart::state::AsyncMode;
+use lpc8xx_hal::{cortex_m::interrupt, gpio, prelude::*};
 use rtic::Mutex;
+use rtt_target::rprintln;
 use void::Void;
-use crate::{
-    handle_pin_interrupt_noint_dynamic,
-    RTS_PIN_NUMBER,
-    handle_pin_interrupt_dynamic,
-    PININT0_DYN_PIN,
-    handle_pin_interrupt,
-    RED_LED_PIN_NUMBER,
-    CTS_PIN_NUMBER,
-};
 
 pub fn handle_idle(cx: crate::idle::Context) -> ! {
-
     let host_rx = cx.resources.host_rx_idle;
     let host_tx = cx.resources.host_tx;
     let target_rx = cx.resources.target_rx_idle;
@@ -35,14 +27,13 @@ pub fn handle_idle(cx: crate::idle::Context) -> ! {
     let blue_idle = cx.resources.blue_idle;
     let pinint0_idle = cx.resources.pinint0_idle;
     let target_rts_idle = cx.resources.target_rts_idle;
-    let pinint0_pin  = cx.resources.pinint0_pin;
+    let pinint0_pin = cx.resources.pinint0_pin;
     let mut dyn_noint_pins = cx.resources.dyn_noint_pins;
     let dyn_noint_levels_out = cx.resources.dyn_noint_levels_out;
-    let cts          = cx.resources.cts;
-    let rts          = cx.resources.rts;
-    let pwm          = cx.resources.pwm_idle;
-    let pin_5        = cx.resources.pin_5;
-
+    let cts = cx.resources.cts;
+    let rts = cx.resources.rts;
+    let pwm = cx.resources.pwm_idle;
+    let pin_5 = cx.resources.pin_5;
 
     let mut fixed_pin_levels = FnvIndexMap::<_, _, U8>::new();
     let mut dynamic_int_pin_levels = FnvIndexMap::<_, _, U4>::new();
@@ -80,9 +71,7 @@ pub fn handle_idle(cx: crate::idle::Context) -> ! {
                     HostToAssistant::SendUsart {
                         mode: UsartMode::Regular,
                         data,
-                    } => {
-                        target_tx.send_raw(data)
-                    }
+                    } => target_tx.send_raw(data),
                     HostToAssistant::SendUsart {
                         mode: UsartMode::Dma,
                         data,
@@ -93,21 +82,15 @@ pub fn handle_idle(cx: crate::idle::Context) -> ! {
                     HostToAssistant::SendUsart {
                         mode: UsartMode::FlowControl,
                         data: _,
-                    } => {
-                        Ok(())
-                    }
+                    } => Ok(()),
                     HostToAssistant::SendUsart {
                         mode: UsartMode::Sync,
                         data,
-                    } => {
-                        target_sync_tx.send_raw(data)
-                    }
-                    HostToAssistant::SetPin(
-                        pin::SetLevel {
-                            pin: OutputPin::Pin5,
-                            level,
-                        }
-                    ) => {
+                    } => target_sync_tx.send_raw(data),
+                    HostToAssistant::SetPin(pin::SetLevel {
+                        pin: OutputPin::Pin5,
+                        level,
+                    }) => {
                         match level {
                             pin::Level::High => {
                                 pin_5.set_high();
@@ -118,22 +101,12 @@ pub fn handle_idle(cx: crate::idle::Context) -> ! {
                         }
                         Ok(())
                     }
-                    HostToAssistant::SetPin(
-                        pin::SetLevel {
-                            pin: _,
-                            level: _,
-                        }
-                    ) => {
+                    HostToAssistant::SetPin(pin::SetLevel { pin: _, level: _ }) => {
                         // currently we don't have defined any other non-dynamic Output Pins that could be set
                         // TODO refactor back: this should be usable
                         unreachable!()
                     }
-                    HostToAssistant::SetDynamicPin(
-                        pin::SetLevel {
-                            pin,
-                            level,
-                        }
-                    ) => {
+                    HostToAssistant::SetDynamicPin(pin::SetLevel { pin, level }) => {
                         let pin_number = pin.get_pin_number().unwrap();
 
                         // TODO less repetitive once we've unified pin types
@@ -142,32 +115,35 @@ pub fn handle_idle(cx: crate::idle::Context) -> ! {
                             RED_LED_PIN_NUMBER => {
                                 if pinint0_pin.direction_is_output() {
                                     match level {
-                                        pin::Level::High => { pinint0_pin.set_high() }
-                                        pin::Level::Low => { pinint0_pin.set_low() }
+                                        pin::Level::High => pinint0_pin.set_high(),
+                                        pin::Level::Low => pinint0_pin.set_low(),
                                     }
                                 }
                             }
                             CTS_PIN_NUMBER => {
                                 if cts.direction_is_output() {
                                     match level {
-                                        pin::Level::High => { cts.set_high() }
-                                        pin::Level::Low => { cts.set_low() }
+                                        pin::Level::High => cts.set_high(),
+                                        pin::Level::Low => cts.set_low(),
                                     }
-                            }}
-                            RTS_PIN_NUMBER => { unreachable!() }
+                                }
+                            }
+                            RTS_PIN_NUMBER => {
+                                unreachable!()
+                            }
                             pin_number => {
-                                dyn_noint_pins.lock(|pin_map|{ // TODO turn this into a macro?
+                                dyn_noint_pins.lock(|pin_map| {
+                                    // TODO turn this into a macro?
                                     if pin_map.contains_key(&pin_number) {
                                         let pin = pin_map.get_mut(&pin_number).unwrap();
                                         if pin.direction_is_output() {
                                             match level {
-                                                pin::Level::High => { pin.set_high() }
-                                                pin::Level::Low => { pin.set_low() }
+                                                pin::Level::High => pin.set_high(),
+                                                pin::Level::Low => pin.set_low(),
                                             }
                                         }
-                                        // TODO add pin level to buffer right away
-                                    }
-                                    else {
+                                    // TODO add pin level to buffer right away
+                                    } else {
                                         rprintln!("can't query unsupported pin: {:?}", pin_number);
                                     }
                                 })
@@ -176,34 +152,27 @@ pub fn handle_idle(cx: crate::idle::Context) -> ! {
 
                         Ok(())
                     }
-                    HostToAssistant::ReadPin(
-                        pin::ReadLevel { pin }
-                    ) => {
-                        let result = fixed_pin_levels.get(&(pin as usize))
-                            .map(|&(level, period_ms)| {
-                                pin::ReadLevelResult {
+                    HostToAssistant::ReadPin(pin::ReadLevel { pin }) => {
+                        let result =
+                            fixed_pin_levels
+                                .get(&(pin as usize))
+                                .map(|&(level, period_ms)| pin::ReadLevelResult {
                                     pin,
                                     level,
                                     period_ms,
-                                }
-                            });
+                                });
 
                         host_tx
-                            .send_message(
-                                &AssistantToHost::ReadPinResult(result),
-                                &mut buf,
-                            )
+                            .send_message(&AssistantToHost::ReadPinResult(result), &mut buf)
                             .unwrap();
 
                         Ok(())
                     }
-                    HostToAssistant::SetDirection(
-                        pin::SetDirection {
-                            pin,
-                            direction: pin::Direction::Input,
-                            level: None,
-                        }
-                    ) => {
+                    HostToAssistant::SetDirection(pin::SetDirection {
+                        pin,
+                        direction: pin::Direction::Input,
+                        level: None,
+                    }) => {
                         //rprintln!("{:?} is Input", pin);
 
                         match pin.get_pin_number().unwrap() {
@@ -215,50 +184,46 @@ pub fn handle_idle(cx: crate::idle::Context) -> ! {
                                     true => pin::Level::High,
                                     false => pin::Level::Low,
                                 };
-                                dynamic_int_pin_levels.insert(RED_LED_PIN_NUMBER as usize, (pinint0_level, None))
-                                                .unwrap();
-                            },
+                                dynamic_int_pin_levels
+                                    .insert(RED_LED_PIN_NUMBER as usize, (pinint0_level, None))
+                                    .unwrap();
+                            }
                             CTS_PIN_NUMBER => {
                                 // TODO proper error handling
                                 rprintln!("CTS pin is never Input");
                                 unreachable!()
                             }
-                            RTS_PIN_NUMBER => {
-                                rts.switch_to_input()
-                            },
+                            RTS_PIN_NUMBER => rts.switch_to_input(),
                             30 => {
                                 // Ignore for now, we've hardcoded this pin as input
                                 // TODO fix this
                             }
                             pin_number => {
-                                dyn_noint_pins.lock(|pin_map|{
+                                dyn_noint_pins.lock(|pin_map| {
                                     if pin_map.contains_key(&pin_number) {
                                         // this is a dynamic non-interrupt pin, set its direction
                                         let pin = pin_map.get_mut(&pin_number).unwrap();
                                         pin.switch_to_input();
-                                    }
-                                    else {
+                                    } else {
                                         rprintln!("unsupported pin: {:?}", pin_number)
                                     }
                                 });
-                            },
+                            }
                         };
                         Ok(())
-                    },
+                    }
                     // TODO merge this with SetDirection for Input; control flow is duplicate
-                    HostToAssistant::SetDirection(
-                        pin::SetDirection {
-                            pin,
-                            direction: pin::Direction::Output,
-                            level: Some(level),
-                        }
-                    ) => {
+                    HostToAssistant::SetDirection(pin::SetDirection {
+                        pin,
+                        direction: pin::Direction::Output,
+                        level: Some(level),
+                    }) => {
                         //rprintln!("{:?} is Output | Level {:?}", pin, level);
                         // convert from lpc8xx_hal::gpio::Level to protocol::pin::Level
                         // TODO use into() here
                         let gpio_level = match level {
-                            pin::Level::High => {gpio::Level::High}
-                            pin::Level::Low => {gpio::Level::Low}
+                            pin::Level::High => gpio::Level::High,
+                            pin::Level::Low => gpio::Level::Low,
                         };
 
                         // todo nicer and more generic once we start enabling ALL the pins
@@ -272,9 +237,10 @@ pub fn handle_idle(cx: crate::idle::Context) -> ! {
                                     true => pin::Level::High,
                                     false => pin::Level::Low,
                                 };
-                                dynamic_int_pin_levels.insert(RED_LED_PIN_NUMBER as usize, (pinint0_level, None))
-                                                .unwrap();
-                            },
+                                dynamic_int_pin_levels
+                                    .insert(RED_LED_PIN_NUMBER as usize, (pinint0_level, None))
+                                    .unwrap();
+                            }
                             CTS_PIN_NUMBER => cts.switch_to_output(gpio_level),
                             RTS_PIN_NUMBER => {
                                 // TODO proper error handling
@@ -282,7 +248,7 @@ pub fn handle_idle(cx: crate::idle::Context) -> ! {
                                 unreachable!()
                             }
                             pin_number => {
-                                dyn_noint_pins.lock(|pin_map|{
+                                dyn_noint_pins.lock(|pin_map| {
                                     if pin_map.contains_key(&pin_number) {
                                         // this is a dynamic non-interrupt pin, set its direction
                                         let pin = pin_map.get_mut(&pin_number).unwrap();
@@ -291,24 +257,20 @@ pub fn handle_idle(cx: crate::idle::Context) -> ! {
                                         rprintln!("unsupported pin")
                                     }
                                 });
-                            },
+                            }
                         };
                         Ok(())
-                    },
-                    HostToAssistant::SetDirection(
-                        pin::SetDirection {
-                            pin: _,
-                            direction: _,
-                            level: _,
-                        }
-                    ) => {
+                    }
+                    HostToAssistant::SetDirection(pin::SetDirection {
+                        pin: _,
+                        direction: _,
+                        level: _,
+                    }) => {
                         // illegal level/direction combination
                         // TODO handle error more neatly
                         unreachable!()
-                    },
-                    HostToAssistant::ReadDynamicPin(
-                        pin::ReadLevel { pin }
-                    ) => {
+                    }
+                    HostToAssistant::ReadDynamicPin(pin::ReadLevel { pin }) => {
                         // AJM!
                         hdl_read_dynamic_pin(
                             pin,
@@ -327,7 +289,7 @@ pub fn handle_idle(cx: crate::idle::Context) -> ! {
         host_rx.clear_buf();
 
         // TODO: is pwm pin ever handled in reading messages?
-        handle_pin_interrupt(pwm,   InputPin::Pwm,   &mut fixed_pin_levels);
+        handle_pin_interrupt(pwm, InputPin::Pwm, &mut fixed_pin_levels);
         handle_pin_interrupt(blue_idle, InputPin::Blue, &mut fixed_pin_levels);
 
         handle_pin_interrupt_dynamic(pinint0_idle, PININT0_DYN_PIN, &mut dynamic_int_pin_levels);
@@ -372,7 +334,7 @@ fn hdl_read_dynamic_pin(
     // Lotte says: one is pins, one is events of pins
     // TODO(LS): Better naming
     dynamic_noint_pin_levels: &mut FnvIndexMap<usize, gpio::Level, U4>,
-    fixed_pin_levels: &mut FnvIndexMap::<usize, (pin::Level, Option<u32>), U8>,
+    fixed_pin_levels: &mut FnvIndexMap<usize, (pin::Level, Option<u32>), U8>,
     host_tx: &mut Tx<USART0, AsyncMode>,
     buf: &mut [u8],
     dynamic_int_pin_levels: &mut FnvIndexMap<usize, (pin::Level, Option<u32>), U4>,
@@ -382,7 +344,7 @@ fn hdl_read_dynamic_pin(
     // TODO return bool from closure and when I've figured that out also
     // fix pin_is_input
     let mut is_dyn_noint_pin = false;
-    dyn_noint_pins.lock(|pin_map|{
+    dyn_noint_pins.lock(|pin_map| {
         is_dyn_noint_pin = pin_map.contains_key(&pin_number);
     });
 
@@ -392,43 +354,34 @@ fn hdl_read_dynamic_pin(
             // TODO don't hardcode this!
             fixed_pin_levels
                 .get(&(InputPin::Blue as usize))
-                .map(|&(level, period_ms)| {
-                    pin::ReadLevelResult {
-                        pin,
-                        level,
-                        period_ms,
-                    }
+                .map(|&(level, period_ms)| pin::ReadLevelResult {
+                    pin,
+                    level,
+                    period_ms,
                 })
         }
         (pin_number, true) => {
             dynamic_noint_pin_levels
-            .get(&(pin_number as usize))
-            .map(|gpio_level| {
-                pin::ReadLevelResult {
+                .get(&(pin_number as usize))
+                .map(|gpio_level| pin::ReadLevelResult {
                     pin,
                     level: pin::Level::from(*gpio_level),
                     period_ms: None,
-                }
-            })
+                })
         }
         (pin_number, false) => {
             dynamic_int_pin_levels
-            .get(&(pin_number as usize))
-            .map(|&(level, period_ms)| {
-                pin::ReadLevelResult {
+                .get(&(pin_number as usize))
+                .map(|&(level, period_ms)| pin::ReadLevelResult {
                     pin,
                     level,
                     period_ms,
-                }
-            })
+                })
         }
     };
 
     host_tx
-        .send_message(
-            &AssistantToHost::ReadPinResultDynamic(result),
-            buf,
-        )
+        .send_message(&AssistantToHost::ReadPinResultDynamic(result), buf)
         .unwrap();
 
     Ok(())
