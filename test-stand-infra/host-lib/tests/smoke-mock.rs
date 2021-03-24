16b5292f53760732432340f17bf8fb0bf3437164
James Munns <james.munns@ferrous-systems.com>
2021-03-24 11:08:20 +0100

    First snapshot test

--- test-stand-infra/host-lib/tests/smoke-mock.rs
+++ test-stand-infra/host-lib/tests/smoke-mock.rs
@@ -4,13 +4,15 @@ use host_lib::{
     conn::Conn,
 };
 
+use insta::assert_debug_snapshot;
+
 #[test]
 fn make_instance() {
     let mock = Mock::new();
     let test_hdl = mock.clone();
 
     let conn = Conn::from_serial_port(Box::new(mock)).unwrap();
-    let assistant = Assistant::new(conn);
+    let _assistant = Assistant::new(conn);
 
     assert!(test_hdl.is_totally_empty());
 }
@@ -26,7 +28,10 @@ fn set_pin_5_high() {
 
     // TODO: Use the protocol to deserialize?
     // Add insta for snapshot tests
-    assert_eq!(b"", test_hdl.pop_host_lib_data().unwrap().as_slice());
+    assert_debug_snapshot!(
+        "set_pin_5_high",
+        test_hdl.pop_host_lib_data().unwrap()
+    );
 
     assert!(test_hdl.is_totally_empty());
 }
