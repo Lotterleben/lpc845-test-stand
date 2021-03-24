6edbc2f111152becafd9c4dbce2bb48a1c8ac3bc
James Munns <james.munns@ferrous-systems.com>
2021-03-24 11:17:11 +0100

    Explicitly check bytes AND deserialized representation

--- test-stand-infra/host-lib/tests/smoke-mock.rs
+++ test-stand-infra/host-lib/tests/smoke-mock.rs
@@ -3,6 +3,8 @@ use host_lib::{
     assistant::Assistant,
     conn::Conn,
 };
+use protocol::HostToAssistant;
+use postcard;
 
 use insta::assert_debug_snapshot;
 
@@ -26,11 +28,20 @@ fn set_pin_5_high() {
     let mut assistant = Assistant::new(conn);
     assistant.set_pin_5_high().unwrap();
 
+    let msg = test_hdl.pop_host_lib_data().unwrap();
+    let mut msg_clone = msg.clone();
+    let deser_msg: HostToAssistant = postcard::from_bytes_cobs(&mut msg_clone).unwrap();
+
     // TODO: Use the protocol to deserialize?
     // Add insta for snapshot tests
     assert_debug_snapshot!(
-        "set_pin_5_high",
-        test_hdl.pop_host_lib_data().unwrap()
+        "set_pin_5_high - bytes",
+        &msg
+    );
+
+    assert_debug_snapshot!(
+        "set_pin_5_high - parsed",
+        &deser_msg
     );
 
     assert!(test_hdl.is_totally_empty());
