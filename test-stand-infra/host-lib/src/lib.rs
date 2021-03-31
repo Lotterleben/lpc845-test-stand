e911db5054b6dce6c8b6b98762b298a4b967598b
James Munns <james.munns@ferrous-systems.com>
2021-03-31 11:13:50 +0200

    Add compile error notes

--- test-stand-infra/host-lib/src/lib.rs
+++ test-stand-infra/host-lib/src/lib.rs
@@ -11,6 +11,22 @@ pub mod pin;
 pub mod test_stand;
 
 
+compile_error!("""
+    Notes: 2021-03-31
+
+    We need to begin removing the todo statements, primarily by
+    using a match statement to convert the internal DynamicPin Token
+    into ephemeral instances of InputPin or OutputPin in order to
+    create the correct type for serialization
+
+    Lotte: Replace our local DynamicPin with a HAL DynamicPin?
+    James: What about other HALs?
+    Lotte: Can we make this a trait/generic instead? Use ones we have?
+    James: This requires dynamic dispatch, but that might be okay
+
+    Consider doing this instead of manual matching everywhere
+""");
+
 pub use self::{
     assistant::Assistant,
     config::Config,
